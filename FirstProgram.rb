7735141d66bebdd14d763ccd54c9ea5d825ebf4e
MinhNamThan <thanminhnam17022001@gmail.com>
2022-06-24 14:01:06 +0700

    add new branch file

---
+++ FirstProgram.rb
@@ -0,0 +1,8 @@
+puts "hello world"
+print "hello\nworld!!"
+puts "h\nl"
+5.times{print"I love you!\n"}
+5.times{puts"I hate you\n"}
+puts "9+8 = #{9+8} and " + "1 - 2 = #{1-2}"
+print "1 + 3 = #{1+3}\n"
+puts "1+3 = #{1+3}"
